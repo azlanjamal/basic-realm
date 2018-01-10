0f5f45ea2f07d7c5e37ba4a5fa69900ac07230db
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-10 11:11:22 +0800

    Handle layout method in class.

--- app/src/main/java/com/azlanjamal/basicrealm/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicrealm/MainActivity.java
@@ -2,12 +2,56 @@ package com.azlanjamal.basicrealm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import io.realm.Realm;
+import io.realm.RealmAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText etPersonName, etAge, etSocialAccountName, etStatus;
+
+    private Realm myRealm;
+    private RealmAsyncTask realmAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etPersonName 		= (EditText) findViewById(R.id.etPersonName);
+        etAge 				= (EditText) findViewById(R.id.etAge);
+        etSocialAccountName = (EditText) findViewById(R.id.etSocialAccount);
+        etStatus 			= (EditText) findViewById(R.id.etStatus);
+
+    }
+
+    // Add data to Realm using Main UI Thread. Be Careful: As it may BLOCK the UI.
+    public void addUserToRealm_Synchronously(View view) {
+
+    }
+
+    // Add Data to Realm in the Background Thread.
+    public void addUserToRealm_ASynchronously(View view) {
+
+    }
+
+    public void displayAllUsers(View view) {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
