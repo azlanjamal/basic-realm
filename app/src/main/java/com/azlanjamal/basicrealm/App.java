3d815d4da65cc884bf3dd963886a34532e06a337
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-10 11:43:50 +0800

    Configure realm.

--- app/src/main/java/com/azlanjamal/basicrealm/App.java
+++ app/src/main/java/com/azlanjamal/basicrealm/App.java
@@ -2,9 +2,25 @@ package com.azlanjamal.basicrealm;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by akmalazlan on 10/01/2018.
  */
 
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+
+        RealmConfiguration configuration = new RealmConfiguration.Builder()
+                .name("realmBasic.realm")
+                .build();
+
+        Realm.setDefaultConfiguration(configuration);
+    }
 }
