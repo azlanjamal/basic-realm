838cd5f5b94eb827ea7f351ab3ca5f75c406cc4d
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-10 11:44:51 +0800

    Implement add method synchronously.

--- app/src/main/java/com/azlanjamal/basicrealm/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicrealm/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.azlanjamal.basicrealm.model.SocialAccount;
+import com.azlanjamal.basicrealm.model.User;
+
+import java.util.UUID;
 
 import io.realm.Realm;
 import io.realm.RealmAsyncTask;
@@ -27,10 +33,39 @@ public class MainActivity extends AppCompatActivity {
         etSocialAccountName = (EditText) findViewById(R.id.etSocialAccount);
         etStatus 			= (EditText) findViewById(R.id.etStatus);
 
+        myRealm = Realm.getDefaultInstance();
     }
 
     // Add data to Realm using Main UI Thread. Be Careful: As it may BLOCK the UI.
     public void addUserToRealm_Synchronously(View view) {
+
+        final String id = UUID.randomUUID().toString();
+        final String name 				= etPersonName.getText().toString();
+        final int age 					= Integer.valueOf(etAge.getText().toString());
+        final String socialAccountName 	= etSocialAccountName.getText().toString();
+        final String status 				= etStatus.getText().toString();
+
+//        try {
+//            myRealm.beginTransaction();
+//            myRealm.commitTransaction();
+//        } catch (Exception e) {
+//            myRealm.cancelTransaction();
+//        }
+
+        myRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                SocialAccount socialAccount = realm.createObject(SocialAccount.class);
+                socialAccount.setName(socialAccountName);
+                socialAccount.setStatus(status);
+
+                User user = realm.createObject(User.class, id);
+                user.setName(name);
+                user.setAge(age);
+                user.setSocialAccount(socialAccount);
+                Toast.makeText(MainActivity.this, "Added Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
